60cd9a39a858f9004bb07c2bebc3de6b46bd460a
saieesha <saieeshabonthala@gmail.com>
2024-09-12 20:00:39 -0500

    Initial commit-setup 2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items : WishItem[]= [
+    new WishItem('Learn Angular', true),
+    new WishItem('Drink Coffee'),
+    new WishItem('Go on a hike')
+  ];
   title = 'wishlist';
+
+  toggleItem(item : WishItem){
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }

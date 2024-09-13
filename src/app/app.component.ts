77bc229d8c9c4c19260cae501a12e7806e11cb4d
saieesha <saieeshabonthala@gmail.com>
2024-09-12 20:45:05 -0500

    created form to submit wishes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,6 +18,13 @@ export class AppComponent {
     new WishItem('Go on a hike')
   ];
   title = 'wishlist';
+
+  newWishText = '';
+
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
 
   toggleItem(item : WishItem){
     item.isComplete = !item.isComplete;

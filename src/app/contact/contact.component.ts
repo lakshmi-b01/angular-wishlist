14c1d435c30e7717b08212d4d21a2d31e09d62b5
saieesha <saieeshabonthala@gmail.com>
2024-09-16 21:20:54 -0500

    Creating custom validators

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { invalidEmailDomain } from './invalidEmailDomain';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
     ReactiveFormsModule, 
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
@@ -17,7 +18,7 @@ export class ContactComponent {
 
   contactForm = new FormGroup({
     senderName: new FormControl('', Validators.required),
-    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderEmail: new FormControl('', [Validators.required, Validators.email, invalidEmailDomain]),
     senderMessage: new FormControl('', [Validators.required, Validators.minLength(10)]),
   });
 
